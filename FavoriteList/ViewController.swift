a57f80dad31861adf9796ecc0f1e1a5f792665e5
Justin Haung <justinhuang@noodoe.com.tw>
2022-07-19 20:07:15 +0800

    First init

---
+++ FavoriteList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoriteList
+//
+//  Created by Justin Haung on 2022/7/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
