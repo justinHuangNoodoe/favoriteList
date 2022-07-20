f0ed51430ec31e1a761ed90238f27a38c6a3ec0c
Justin Haung <justinhuang@noodoe.com.tw>
2022-07-20 09:55:40 +0800

    Setup IBOutlet & tableView in TopListViewController

--- FavoriteList/TopListViewController.swift
+++ FavoriteList/TopListViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 protocol TopListVCDelegate: AnyObject {}
 
 class TopListViewController: UIViewController, Loadable {
-    
+    @IBOutlet weak var typeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var filterSeqmentedControl: UISegmentedControl!
+    @IBOutlet weak var listTableView: UITableView!
     weak var delegate: TopListVCDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    private func setupUI() {
+        listTableView.register(ListItemTableViewCell.loadNib(), forCellReuseIdentifier: ListItemTableViewCell.identifier)
+        listTableView.delegate = self
+        listTableView.dataSource = self
+        
     }
 }
 
+extension TopListViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Top Movies"
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(indexPath: indexPath) as ListItemTableViewCell
+        cell.titleLabel.text = "標題"
+        cell.rankLabel.text = "排名"
+        cell.startDateLabel.text = "開始時間"
+        cell.endDateLabel.text = "結束時間"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
