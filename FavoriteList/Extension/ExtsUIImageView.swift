a736cb4d667dd5020e51bc8b249a70fd7ff425bc
Justin Haung <justinhuang@noodoe.com.tw>
2022-07-20 19:30:04 +0800

    Add ImageCacheManager

--- FavoriteList/Extension/ExtsUIImageView.swift
+++ FavoriteList/Extension/ExtsUIImageView.swift
@@ -13,15 +13,40 @@ extension UIImageView {
             print("Image url is invalid")
             return
         }
-        print("Download Started")
-        RESTfulService.download(from: url) { [weak self] data, response, error in
-            guard
-                let self = self,
-                let data = data, error == nil else { return }
-            print("Download Finished")
+        if let cacheImage = ImageCacheManager.shared.cacheImage[url.description] {
             DispatchQueue.main.async() {
-                self.image = UIImage(data: data)
+                self.image = cacheImage
+            }
+            print("Use cache Image")
+            return
+        } else {
+            print("Download Started")
+            RESTfulService.download(from: url) { [weak self] data, response, error in
+                guard
+                    let self = self,
+                    let data = data, error == nil else { return }
+                print("Download Finished")
+                DispatchQueue.main.async() {
+                    if let newImage = UIImage(data: data) {
+                        self.image = newImage
+                        ImageCacheManager.shared.setImage(url: url.description, image: newImage)
+                    } else {
+                        print("Data is not image")
+                    }
+                }
             }
         }
+    }
+}
+
+class ImageCacheManager {
+    static let shared = ImageCacheManager()
+    private(set) var cacheImage: [String: UIImage]
+    private init() {
+        cacheImage = [:]
+    }
+    
+    func setImage(url: String, image: UIImage) {
+        self.cacheImage[url] = image
     }
 }
